5cc8d0eeba1b64e1689675c3cab7be9a9ff62a62
Tony <98559889+Acromic@users.noreply.github.com>
2022-05-29 09:40:57 -0700

    Delete MechaCarChallenge.RSript.R

--- MechaCarChallenge.RSript.R
+++
@@ -1,9 +0,0 @@
-library(dplyr) # load dplyr
-
-mechaCarDf <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F) # load csv
-
-# head(mechaCarDf)
-
-lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mechaCarDf ) # load lm function
-
-summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mechaCarDf))  #summarize statistics
